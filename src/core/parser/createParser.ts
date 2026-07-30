446c41d865148a04d02e1f079dcb2d80b153880c
Nikhil Github <nikhild12377@gmail.com>
2026-07-30 07:53:46 +0530

    feat: added create parser parser

---
+++ src/core/parser/createParser.ts
@@ -0,0 +1,18 @@
+import { CSVParser } from './CsvParserClass';
+import { ExcelParser } from './ExcelParserClass';
+
+export function createParser(file: File, worker?: boolean) {
+  const extension = file.name.split('.').pop()?.toLowerCase();
+
+  switch (extension) {
+    case 'csv':
+      return new CSVParser(file, worker).parse();
+
+    case 'xlsx':
+    case 'xls':
+      return new ExcelParser(file).parse();
+
+    default:
+      throw new Error(`Unsupported file type: ${extension}`);
+  }
+}
